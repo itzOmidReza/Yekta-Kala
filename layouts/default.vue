dd983de8aba74f9ee823080bf6cf7ef597dec4d6
Omidreza <144906057+OmidRezaWeb@users.noreply.github.com>
2025-07-07 22:31:41 +0330

    chore(project-setup): configure layout, index page, assets and Nuxt config

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,47 @@
 <template>
-  <div>
-    <p>Some default layout content shared across all pages</p>
-    <slot />
+  <div class="page-wrapper">
+    <!-- start of page-header -->
+    <AppHeader />
+    <!-- end of page-header -->
+    <AppMobileHeader />
+    <!-- start of page-content -->
+    <main class="page-content">
+      <div class="container">
+        <slot />
+      </div>
+      <!-- start of quick-view-modal -->
+
+      <!-- end of quick-view-modal -->
+    </main>
+    <!-- end of page-content -->
+    <AppFooter />
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  meta: [{ name: "viewport", content: "width=device-width, initial-scale=1" }],
+
+  link: [{ rel: "stylesheet", href: "/css/dependencies.css" }],
+
+  script: [
+    { src: "js/dependencies/bootstrap-slider.min.js" },
+    { src: "js/dependencies/bootstrap.bundle.min.js" },
+    { src: "js/dependencies/fancybox.umd.js" },
+    { src: "js/dependencies/iziToast.min.js" },
+    { src: "js/dependencies/jquery-3.6.0.min.js" },
+    { src: "js/dependencies/jquery.countdown.min.js" },
+    { src: "js/dependencies/jquery.simple.timer.min.js" },
+    { src: "js/dependencies/nouislider.min.js" },
+    { src: "js/dependencies/remodal.min.js" },
+    { src: "js/dependencies/select2.min.js" },
+    { src: "js/dependencies/simplebar.min.js" },
+    { src: "js/dependencies/swiper-bundle.min.js" },
+    { src: "js/dependencies/wNumb.js" },
+    { src: "js/dependencies/zoomsl.min.js" },
+    { src: "js/theme.js" },
+    { src: "js/custome.js" },
+
+  ],
+});
+</script>

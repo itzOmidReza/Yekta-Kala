7c91e14d5bfabbf1b03abf4e4b68bfdcbee13192
Omidreza <144906057+OmidRezaWeb@users.noreply.github.com>
2025-07-28 18:59:27 +0330

    feat(components):add base Button, install vuevalidate and yup and add test page

---
+++ components/base/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button
+    :class="[
+      'btn',
+      { 'btn-sm': size == 'sm' },
+      { 'btn-lg': size == 'lg' },
+      { 'w-100': wFull },
+      outLine ? `btn-outline-${color}` : `btn-${color}`,
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+import { boolean } from "yup";
+
+defineProps({
+  size: {
+    default: "normal",
+    type: String,
+  },
+  wFull: {
+    default: false,
+    type: Boolean,
+  },
+  outLine: {
+    default: false,
+    type: Boolean,
+  },
+  color: {
+    default: "primary",
+    type: String,
+  },
+});
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <BaseButton size="sm"  out-line w-full >button</BaseButton>
+    </div>
+</template>
